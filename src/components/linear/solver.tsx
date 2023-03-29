42b6c828337a2af4f97a1ea7b8f9047daee48681
종박 <78944379+jongpark1234@users.noreply.github.com>
2023-03-29 14:10:08 +0900

    :art: Improve structure
    + Implemented String Calculator -> eval Function
    + Improve Equation Solving Structure

--- src/components/linear/solver.tsx
+++ src/components/linear/solver.tsx
@@ -1,36 +1,25 @@
-import Calculator from "../../utils/calculator"
-
-const eps = 5e-7
-
-const gcd = (a: number, b: number): number => {
-    return b === 0 ? a : gcd(b, a % b)
-}
-
 const solver = (f: string): string => {
-    const x = 10 ** 10
-    if (!f.includes('=')) {
-        return ''
-    }
-    const term = f.replace('x', () => {
-        return '(10' + '*10'.repeat(9) + ')'
-    }).split('=')
-    const lt = Calculator(term[0])
-    const rt = Calculator(term[1])
-    if (lt === null || rt === null) {
+    try {
+        if (!f.includes('=')) {
+            return 'Not Equation.'
+        }
+        const term = f.split('=')
+        const equation = term[0] + '-' + '(' + term[1] + ')'
+        const a = eval(equation.replace('x', '1'))
+        const b = eval(equation.replace('x', '2'))
+        console.log(a, b)
+        if (a === '0' && b === '0') {
+            return 'Infinitely Many Solutions.'
+        } else if (a === b) {
+            return 'No solution.'
+        } else {
+            const c = eval(equation.replace('x', '0'))
+            console.log(c)
+            const diff = Number(b) - Number(a)
+            return c !== 0 && diff !== 0 ? String(-1 * (Number(c) / diff)) : '0'
+        }
+    } catch {
         return 'Wrong Expression.'
-    }
-    const n1 = Number((Number(lt) / x)) - Number((Number(rt) / x))
-    const n2 = (Number(rt) % x) - (Number(lt) % x)
-    console.log(n1, n2)
-    if (n1 === 0 && n2 !== 0) {
-        return 'No solution.'
-    } else if (n1 === 0 && n2 === 0) {
-        return 'Infinitely many solutions.'
-    } else {
-        const GCD = gcd(Math.round(n1), Math.round(n2))
-        const deno = Math.round(Number(n1 / GCD) + eps)
-        const mole = Math.round(Number(n2 / GCD) + eps)
-        return deno === 1 || deno === -1 ? `${mole * deno}` : `${mole} / ${deno}`
     }
 }
 
