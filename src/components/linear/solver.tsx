b5a20f6e0e9df9aa7c179399dac1b015c16f5c7a
종박 <78944379+jongpark1234@users.noreply.github.com>
2023-03-29 16:27:01 +0900

    :bug: bug fix
    + replace -> replaceAll

--- src/components/linear/solver.tsx
+++ src/components/linear/solver.tsx
@@ -5,21 +5,21 @@ const solver = (f: string): string => {
         }
         const term = f.split('=')
         const equation = term[0] + '-' + '(' + term[1] + ')'
-        const a = eval(equation.replace('x', '1'))
-        const b = eval(equation.replace('x', '2'))
+        const a = eval(equation.replaceAll('x', '1'))
+        const b = eval(equation.replaceAll('x', '2'))
         console.log(a, b)
         if (a === '0' && b === '0') {
             return 'Infinitely Many Solutions.'
         } else if (a === b) {
             return 'No solution.'
         } else {
-            const c = eval(equation.replace('x', '0'))
+            const c = eval(equation.replaceAll('x', '0'))
             console.log(c)
             const diff = Number(b) - Number(a)
             return c !== 0 && diff !== 0 ? String(-1 * (Number(c) / diff)) : '0'
         }
-    } catch {
-        return 'Wrong Expression.'
+    } catch (err) {
+        return String(err)
     }
 }
 
