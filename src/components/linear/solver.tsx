bead363c9d1d6a9f34488fd3d891f811343b9f63
JongPark <jongpark1234@naver.com>
2023-04-13 09:56:25 +0900

    :art: Format of the code
    Using Type Assertion

--- src/components/linear/solver.tsx
+++ src/components/linear/solver.tsx
@@ -5,18 +5,16 @@ const solver = (f: string): string => {
         }
         const term = f.split('=')
         const equation = term[0] + '-' + '(' + term[1] + ')'
-        const a = eval(equation.replaceAll('x', '1'))
-        const b = eval(equation.replaceAll('x', '2'))
-        console.log(a, b)
-        if (a === '0' && b === '0') {
+        const a = eval(equation.replaceAll('x', '1')) as number
+        const b = eval(equation.replaceAll('x', '2')) as number
+        if (a === 0 && b === 0) {
             return 'Infinitely Many Solutions.'
         } else if (a === b) {
             return 'No solution.'
         } else {
-            const c = eval(equation.replaceAll('x', '0'))
-            console.log(c)
-            const diff = Number(b) - Number(a)
-            return c !== 0 && diff !== 0 ? String(-1 * (Number(c) / diff)) : '0'
+            const c = eval(equation.replaceAll('x', '0')) as number
+            const diff = b - a
+            return c !== 0 && diff !== 0 ? String(-1 * (c / diff)) : '0'
         }
     } catch (err) {
         return String(err)
